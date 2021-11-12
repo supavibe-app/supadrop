28df0c05beac3c00f745457032e5e0e171e5c882
Ahmad Taufiq <ataufiq665@gmail.com>
2021-11-13 05:55:02 +0700

    add time when anyone place a bid at last 15 minutes

--- packages/common/src/contexts/meta/meta.tsx
+++ packages/common/src/contexts/meta/meta.tsx
@@ -19,7 +19,7 @@ import {
   pullStoreMetadata,
 } from '.';
 import { StringPublicKey, TokenAccount, useUserAccounts } from '../..';
-import {supabase} from '../../supabaseClient'
+import { supabase } from '../../supabaseClient';
 import moment from 'moment';
 
 const MetaContext = React.createContext<MetaContextState>({
@@ -30,23 +30,29 @@ const MetaContext = React.createContext<MetaContextState>({
   isBidPlaced: false,
   liveDataAuctions: {},
   allDataAuctions: {},
-  dataCollection: new Collection('','','',0,0,0,0,[]),
+  dataCollection: new Collection('', '', '', 0, 0, 0, 0, []),
   // @ts-ignore
   update: () => [AuctionData, BidderMetadata, BidderPot],
   // @ts-ignore
-  updateLiveDataAuction: function (){},
-  updateAllDataAuction: function (){},
-
+  updateLiveDataAuction: function () {},
+  updateAllDataAuction: function () {},
+  updateDetailAuction: function () {},
 });
 
 export function MetaProvider({ children = null as any }) {
   const connection = useConnection();
   const { isReady, storeAddress } = useStore();
-  const [dataCollection,setDataCollection] = useState<Collection>(new Collection('','','',0,0,0,0,[]))
-  const [endingTime, setEndingTime] = useState(0)
+  const [dataCollection, setDataCollection] = useState<Collection>(
+    new Collection('', '', '', 0, 0, 0, 0, []),
+  );
+  const [endingTime, setEndingTime] = useState(0);
   const [state, setState] = useState<MetaState>(getEmptyMetaState());
-  const [liveDataAuctions,setLiveDataAuction] = useState<{[key:string]:ItemAuction}>({})
-  const [allDataAuctions,setAllDataAuction] = useState<{[key:string]:ItemAuction}>({})
+  const [liveDataAuctions, setLiveDataAuction] = useState<{
+    [key: string]: ItemAuction;
+  }>({});
+  const [allDataAuctions, setAllDataAuction] = useState<{
+    [key: string]: ItemAuction;
+  }>({});
   const [page, setPage] = useState(0);
   const [metadataLoaded, setMetadataLoaded] = useState(false);
   const [lastLength, setLastLength] = useState(0);
@@ -160,58 +166,160 @@ export function MetaProvider({ children = null as any }) {
   }
 
   async function updateLiveDataAuction() {
-    supabase.from('auction_status')
-      .select(`
+    supabase
+      .from('auction_status')
+      .select(
+        `
     *,
     nft_data (
       *
     )
-    `)
-    .gt('end_auction',moment().unix())
-    .order('end_auction',{ascending:false})
-    .then(dataAuction => {
-      let listData : {[key:string]:ItemAuction} =  {}
-      if (dataAuction.body != null && dataAuction.body.length > 0) {
-        dataAuction.body.forEach( v =>{
-          listData[v.id] = new ItemAuction(v.id, v.nft_data.name,v.id_nft,v.token_mint,v.price_floor,v.nft_data.img_nft, v.start_auction, v.end_auction, v.highest_bid, v.price_tick, v.gap_time, v.tick_size_ending_phase, v.vault,v.nft_data.arweave_link,v.owner,v.nft_data.mint_key, v.type_auction)
-        })
-        setEndingTime(dataAuction.body[dataAuction.body.length-1].end_auction)
-        setLiveDataAuction(listData)
-      }
-      
-    })
+    `,
+      )
+      .gt('end_auction', moment().unix())
+      .order('end_auction', { ascending: false })
+      .then(dataAuction => {
+        let listData: { [key: string]: ItemAuction } = {};
+        if (dataAuction.body != null && dataAuction.body.length > 0) {
+          dataAuction.body.forEach(v => {
+            listData[v.id] = new ItemAuction(
+              v.id,
+              v.nft_data.name,
+              v.id_nft,
+              v.token_mint,
+              v.price_floor,
+              v.nft_data.img_nft,
+              v.start_auction,
+              v.end_auction,
+              v.highest_bid,
+              v.price_tick,
+              v.gap_time,
+              v.tick_size_ending_phase,
+              v.vault,
+              v.nft_data.arweave_link,
+              v.owner,
+              v.nft_data.mint_key,
+              v.type_auction,
+            );
+          });
+          setEndingTime(
+            dataAuction.body[dataAuction.body.length - 1].end_auction,
+          );
+          setLiveDataAuction(listData);
+        }
+      });
+  }
+  async function updateDetailAuction(idAuction: string) {
+    supabase
+      .from('auction_status')
+      .select(
+        `
+    *,
+    nft_data (
+      *
+    )
+    `,
+      )
+      .eq('id', idAuction)
+      .single()
+      .then(dataAuction => {
+        if (dataAuction.body != null && dataAuction.body.length > 0) {
+          let detailAuction = new ItemAuction(
+            dataAuction.body.id,
+            dataAuction.body.nft_data.name,
+            dataAuction.body.id_nft,
+            dataAuction.body.token_mint,
+            dataAuction.body.price_floor,
+            dataAuction.body.nft_data.img_nft,
+            dataAuction.body.start_auction,
+            dataAuction.body.end_auction,
+            dataAuction.body.highest_bid,
+            dataAuction.body.price_tick,
+            dataAuction.body.gap_time,
+            dataAuction.body.tick_size_ending_phase,
+            dataAuction.body.vault,
+            dataAuction.body.nft_data.arweave_link,
+            dataAuction.body.owner,
+            dataAuction.body.nft_data.mint_key,
+            dataAuction.body.type_auction,
+          );
+          if (detailAuction.endAt > moment().unix()) {
+            setLiveDataAuction({ ...liveDataAuctions, detailAuction });
+          }
+          setAllDataAuction({ ...allDataAuctions, detailAuction });
+        }
+      });
   }
 
-  async function updateAllDataAuction(){
-    supabase.from('auction_status')
-    .select(`
+  async function updateAllDataAuction() {
+    supabase
+      .from('auction_status')
+      .select(
+        `
     *,
     nft_data (
       *
     )
-    `)
-    .then(dataAuction => {
-      let listData : {[key:string]:ItemAuction} =  {}
-      if (dataAuction.body != null && dataAuction.body.length > 0) {
-        dataAuction.body.forEach( v =>{
-          listData[v.id] = new ItemAuction(v.id, v.nft_data.name,v.id_nft,v.token_mint,v.price_floor,v.nft_data.img_nft, v.start_auction, v.end_auction, v.highest_bid, v.price_tick, v.gap_time, v.tick_size_ending_phase, v.vault,v.nft_data.arweave_link,v.owner,v.nft_data.mint_key, v.type_auction)
-        })
-        setAllDataAuction(listData)
-      }
-    })
+    `,
+      )
+      .then(dataAuction => {
+        let listData: { [key: string]: ItemAuction } = {};
+        if (dataAuction.body != null && dataAuction.body.length > 0) {
+          dataAuction.body.forEach(v => {
+            listData[v.id] = new ItemAuction(
+              v.id,
+              v.nft_data.name,
+              v.id_nft,
+              v.token_mint,
+              v.price_floor,
+              v.nft_data.img_nft,
+              v.start_auction,
+              v.end_auction,
+              v.highest_bid,
+              v.price_tick,
+              v.gap_time,
+              v.tick_size_ending_phase,
+              v.vault,
+              v.nft_data.arweave_link,
+              v.owner,
+              v.nft_data.mint_key,
+              v.type_auction,
+            );
+          });
+          setAllDataAuction(listData);
+        }
+      });
   }
-  async function getDataCollection(){
-    supabase.from('collections')
-    .select(`*`)
-    .eq('id',1)
-    .then(data => {
-      if (data.body != null) {
-        const {id,name,description,supply,price,sold,start_publish,sample_images} = data.body[0]
-        let collection = new Collection(id,name,description,supply,price,sold,start_publish,sample_images)
-        setDataCollection(collection)
-      }
-      
-    })
+  async function getDataCollection() {
+    supabase
+      .from('collections')
+      .select(`*`)
+      .eq('id', 1)
+      .then(data => {
+        if (data.body != null) {
+          const {
+            id,
+            name,
+            description,
+            supply,
+            price,
+            sold,
+            start_publish,
+            sample_images,
+          } = data.body[0];
+          let collection = new Collection(
+            id,
+            name,
+            description,
+            supply,
+            price,
+            sold,
+            start_publish,
+            sample_images,
+          );
+          setDataCollection(collection);
+        }
+      });
   }
 
   async function update(
@@ -236,11 +344,10 @@ export function MetaProvider({ children = null as any }) {
     Promise.all([
       updateLiveDataAuction(),
       updateAllDataAuction(),
-      getDataCollection()
-    ])
-    .finally(()=>{
-      setIsLoadingDatabase(false)
-    })
+      getDataCollection(),
+    ]).finally(() => {
+      setIsLoadingDatabase(false);
+    });
 
     let nextState = await pullPage(connection, page, state);
 
@@ -280,9 +387,7 @@ export function MetaProvider({ children = null as any }) {
         }
 
         const auction = window.location.href.match(/auction\/(\w+)/);
-        const billing = window.location.href.match(
-          /auction\/(\w+)\/billing/,
-        );
+        const billing = window.location.href.match(/auction\/(\w+)\/billing/);
 
         if (auction && page == 0) {
           console.log(
@@ -306,7 +411,7 @@ export function MetaProvider({ children = null as any }) {
           currLastLength = last;
           return last;
         });
-        if (nextState.storeIndexer.length != currLastLength) {
+        if (currLastLength && nextState.storeIndexer.length >= currLastLength) {
           setPage(page => page + 1);
         }
         setLastLength(nextState.storeIndexer.length);
@@ -331,8 +436,6 @@ export function MetaProvider({ children = null as any }) {
       setIsLoadingMetaplex(false);
     }
 
-
-    
     console.log('------->set finished', new Date());
 
     await updateMints(nextState.metadataByMint);
@@ -428,6 +531,7 @@ export function MetaProvider({ children = null as any }) {
         allDataAuctions,
         updateLiveDataAuction,
         updateAllDataAuction,
+        updateDetailAuction,
         isBidPlaced,
         setBidPlaced,
       }}
